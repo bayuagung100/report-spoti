2120f0979dd7f7c2165a5d386db03d71e9474b68
bayuagung100 <bayuagung100@gmail.com>
2020-04-12 15:22:01 +0700

    tambah akun key

--- page-content/akun-key.php
+++ page-content/akun-key.php
@@ -29,26 +29,19 @@ switch ($show) {
                 </div>
     ';  
         
-        buka_datatables(array("Label","Email","Password","Status","Anggota"));
+        buka_datatables(array("Key","Email"));
         $no = 1;
-        $query = $mysqli->query("SELECT * FROM akun_owner ORDER BY id DESC");
+        $query = $mysqli->query("SELECT * FROM akun_key ORDER BY id DESC");
         while ($data = $query->fetch_array()) {
             $id = $data['id'];
-            $label = $data['label'];
+            $key = $data['api_key'];
             $email = $data['email'];
-            $password = $data['password'];
-            $status = $data['status'];
-            $anggota1 = $data['anggota1'];
-            $anggota2 = $data['anggota2'];
-            $anggota3 = $data['anggota3'];
-            $anggota4 = $data['anggota4'];
-            $anggota5 = $data['anggota5'];
             
-            isi_datatables($no, array($label,$email,$password,$status,$anggota1."<br>".$anggota2."<br>".$anggota3."<br>".$anggota4."<br>".$anggota5), $link, $id);
+            isi_datatables($no, array($key,$email), $link, $id);
             $no++;
         }
 
-        tutup_datatables(array("Label","Email","Password","Status","Anggota"));
+        tutup_datatables(array("Key","Email"));
         echo '
             </div>
         </div>
@@ -59,12 +52,12 @@ switch ($show) {
     case "form":
         if (isset($_GET['id'])) {
             $ip = $_GET['id'];
-            $query     = $mysqli->query("SELECT * FROM akun_owner WHERE id='$ip'");
+            $query     = $mysqli->query("SELECT * FROM akun_key WHERE id='$ip'");
             $data = $query->fetch_array();
             $aksi     = "Edit";
         } else {
             $ip = "";
-            $data = array("id" => "", "label" => "", "email" => "", "password" => "", "status" => "", "anggota1" => "", "anggota2" => "", "anggota3" => "", "anggota4" => "", "anggota5" => "");
+            $data = array("id" => "", "api_key" => "", "email" => "");
             $aksi     = "Tambah";
         }
         echo '
@@ -73,46 +66,13 @@ switch ($show) {
             <br>
                 <div class="card ">
                 <div class="card-header">
-                    <h3 class="card-title">'.$aksi.' Akun Owner (<span style="color:red">*</span> wajib diisi.)</h3>
+                    <h3 class="card-title">'.$aksi.' Akun Key (<span style="color:red">*</span> wajib diisi.)</h3>
                 </div>
                 <div class="card-body">';
                 buka_form($link, $data['id'], strtolower($aksi));
-                buat_textbox("Label", "label", $data['label'],"Enter label", "required");
-                buat_inlinebuka();
-                    buat_inlinebuka_col(6);
-                        buat_inline("Email", "email", $data['email'],"Enter email", "required");
-                    buat_inlinetutup_col();
-                    buat_inlinebuka_col(6);
-                        buat_inline("Password", "password", $data['password'],"Enter password", "required");
-                    buat_inlinetutup_col();
-                buat_inlinetutup();
-                buat_inlinebuka();
-                    buat_inlinebuka_col(6);
-                        $list[] = array('val'=>'', 'cap'=>'Tidak Ada');
-                        $list[] = array('val'=>'Aktif', 'cap'=>'Aktif');
-                        $list[] = array('val'=>'Nonaktif', 'cap'=>'Nonaktif');
-                        buat_inline_select("Status akun ", "status", $list, $data['status'], "required");
-                    buat_inlinetutup_col();
-                    buat_inlinebuka_col(6);
-                        buat_inline("Anggota 1", "anggota1", $data['anggota1'],"Enter anggota 1");
-                    buat_inlinetutup_col();
-                buat_inlinetutup();
-                buat_inlinebuka();
-                    buat_inlinebuka_col(6);
-                        buat_inline("Anggota 2", "anggota2", $data['anggota2'],"Enter anggota 2");
-                    buat_inlinetutup_col();
-                    buat_inlinebuka_col(6);
-                        buat_inline("Anggota 3", "anggota3", $data['anggota3'],"Enter anggota 3");
-                    buat_inlinetutup_col();
-                buat_inlinetutup();
-                buat_inlinebuka();
-                    buat_inlinebuka_col(6);
-                        buat_inline("Anggota 4", "anggota4", $data['anggota4'],"Enter anggota 4");
-                    buat_inlinetutup_col();
-                    buat_inlinebuka_col(6);
-                        buat_inline("Anggota 5", "anggota5", $data['anggota5'],"Enter anggota 5");
-                    buat_inlinetutup_col();
-                buat_inlinetutup();
+                buat_textbox("Key", "api_key", $data['api_key'],"Enter key", "required");
+                buat_textbox("Email", "email", $data['email'],"Enter Email");
+                
                 tutup_form($link);
         echo'                
                 </div>
@@ -123,54 +83,26 @@ switch ($show) {
         break;
         
     case "action":
-        $label	= addslashes(ucwords($_POST['label']));
+        $keys	= addslashes($_POST['api_key']);
         $email	= addslashes($_POST['email']);
-        $password	= addslashes($_POST['password']);
-        $status	= addslashes($_POST['status']);
-        $anggota1	= addslashes($_POST['anggota1']);
-        $anggota2	= addslashes($_POST['anggota2']);
-        $anggota3	= addslashes($_POST['anggota3']);
-        $anggota4	= addslashes($_POST['anggota4']);
-        $anggota5	= addslashes($_POST['anggota5']);
                
         if ($_POST['aksi']=="tambah") {
-            $query = $mysqli->query("INSERT INTO akun_owner
+            $query = $mysqli->query("INSERT INTO akun_key
             (
-                label,
-                email,
-                password,
-                status,
-                anggota1,
-                anggota2,
-                anggota3,
-                anggota4,
-                anggota5
+                api_key,
+                email
             )
             VALUES
             (
-                '$label',
-                '$email',
-                '$password',
-                '$status',
-                '$anggota1',
-                '$anggota2',
-                '$anggota3',
-                '$anggota4',
-                '$anggota5'
+                '$keys',
+                '$email'
             )
             ");
         }
         if ($_POST['aksi']=="edit") {
-            $query = $mysqli->query("UPDATE akun_owner SET
-            label = '$label',
-            email = '$email',
-            password = '$password',
-            status = '$status',
-            anggota1 = '$anggota1',
-            anggota2 = '$anggota2',
-            anggota3 = '$anggota3',
-            anggota4 = '$anggota4',
-            anggota5 = '$anggota5'
+            $query = $mysqli->query("UPDATE akun_key SET
+            api_key = '$keys',
+            email = '$email'
             WHERE id='$_POST[id]'
             ");
         }
@@ -178,7 +110,7 @@ switch ($show) {
         break;
     
     case "delete":
-        $query = $mysqli->query("DELETE FROM akun_owner WHERE id='$_GET[id]'");
+        $query = $mysqli->query("DELETE FROM akun_key WHERE id='$_GET[id]'");
         header('location:'.$link);
         break;
 }
